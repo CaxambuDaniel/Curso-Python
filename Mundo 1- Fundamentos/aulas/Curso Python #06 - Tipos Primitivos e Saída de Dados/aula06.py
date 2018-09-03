16b5252ae8b6c4344604b9224b5fa6ce42fd229a
CaxambuDaniel <contatocarvalhos@gmail.com>
2018-09-02 21:52:22 -0300

    Exercicios 003 e 004 feitos.

--- Mundo 1- Fundamentos/aulas/Curso Python #06 - Tipos Primitivos e Saída de Dados/aula06.py
+++ Mundo 1- Fundamentos/aulas/Curso Python #06 - Tipos Primitivos e Saída de Dados/aula06.py
@@ -1,4 +1,4 @@
-n1 = input('digite um numero')
-n2 = input('digite mais um numero')
+n1 = int(input('digite um numero'))
+n2 = int(input('digite mais um numero'))
 s = int(n1) + int(n2)
-print('soma vale {}'.format(s))
+print('soma entre {} e {} vale {}'.format(n1, n2, s))
